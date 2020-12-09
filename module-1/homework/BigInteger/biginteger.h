0344ebdba595f9afefc644d6a246e0ac43365a9a
Grif-cpp <71193593+Grif-cpp@users.noreply.github.com>
2020-12-09 18:45:06 +0300

    Update biginteger.h

--- module-1/homework/BigInteger/biginteger.h
+++ module-1/homework/BigInteger/biginteger.h
@@ -1,15 +1,15 @@
 class bigint
 {
-	public: 
-	int base = 1e9;
+public:
 	std::vector<int>arr;
 	bool is_neg;
+	int base = 1e9;
 	friend bigint operator - (bigint a);
-	friend bigint operator +( bigint a,  bigint b);
+	friend bigint operator +(const bigint a,const bigint b);
 	friend bigint operator -( bigint a,  bigint b);
 	friend bigint operator *(const bigint &a,const bigint &b);
 	friend bigint operator /( bigint &a,  bigint &b);
-	friend bool operator <(bigint &a, bigint &b);
+	friend bool operator <(const bigint &a,const bigint &b);
 	friend bool operator >(bigint &a, bigint &b);
 	friend bool operator <=(bigint &a, bigint &b);
 	friend bool operator >=(bigint &a, bigint &b);
@@ -23,7 +23,7 @@ class bigint
 	friend bigint toBigint(int& a);
 	friend bool toBool(bigint &a);
 	void dig() {
-		for (int i = arr.size() - 1; i >= 0; i--) {
+		for (auto i = arr.size() - 1; i >= 0; i--) {
 			if (arr[i] != 0) {
 				arr.resize(i + 1);
 				break;
