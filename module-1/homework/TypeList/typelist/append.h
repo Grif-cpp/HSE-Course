402b1b17e252642bebac472d628084fd517c4058
Grif-cpp <71193593+Grif-cpp@users.noreply.github.com>
2020-12-21 17:42:09 +0300

    Update append.h

--- module-1/homework/TypeList/typelist/append.h
+++ module-1/homework/TypeList/typelist/append.h
@@ -3,4 +3,14 @@
 #include "typelist.h"
 
 template<typename TList, typename NewType>
-struct Append;
+struct Append {
+	typedef TypeList<typename TList::Head, typename Append<typename TList::Tail, NewType>::NewTypeList> NewTypeList;
+};
+template<typename NewType>
+struct Append<NullType, NewType> {
+	typedef TypeList<NewType, NullType> NewTypeList;
+};
+template<>
+struct Append<NullType, NullType> {
+	typedef NullType NewTypeList;
+};
