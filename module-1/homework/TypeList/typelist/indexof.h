861d4c4ffbe90079b0f24db03396a6208ffa943b
Grif-cpp <71193593+Grif-cpp@users.noreply.github.com>
2020-12-21 17:43:05 +0300

    Update indexof.h

--- module-1/homework/TypeList/typelist/indexof.h
+++ module-1/homework/TypeList/typelist/indexof.h
@@ -4,3 +4,17 @@
 
 template<typename TList, typename TargetType>
 struct IndexOf;
+
+template<typename TargetType>
+struct IndexOf<NullType, TargetType> {
+	const static int ind = -1;
+};
+template<typename Tail, typename TargetType>
+struct IndexOf<TypeList<TargetType, Tail>, TargetType> {
+	const static int ind = 0;
+};
+template<typename Head, typename Tail, typename TargetType>
+struct IndexOf<TypeList<Head, Tail>, TargetType> {
+	const static int next = IndexOf<Tail, TargetType>::ind;
+	const static int ind = (next == -1) ? -1 : next + 1;
+};
