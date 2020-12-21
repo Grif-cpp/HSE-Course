f3f649766a11b716437802ad34b7e8a083c5767b
Grif-cpp <71193593+Grif-cpp@users.noreply.github.com>
2020-12-21 17:43:48 +0300

    Update noduplicates.h

--- module-1/homework/TypeList/typelist/noduplicates.h
+++ module-1/homework/TypeList/typelist/noduplicates.h
@@ -3,5 +3,16 @@
 #include "erase.h"
 #include "typelist.h"
 
-template<typename TList>
+template<typename Tlist>
 struct NoDuplicates;
+
+template<typename TList>
+struct NoDuplicates
+{
+	typedef TypeList<typename TList::Head,
+		typename NoDuplicates<typename EraseAll<typename TList::Tail, typename TList::Head>::NewTypeList>::NewTypeList> NewTypeList;
+};
+template<>
+struct NoDuplicates<NullType> {
+	typedef NullType NewTypeList;
+};
