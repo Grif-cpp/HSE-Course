0749fde0449df033683bba31dbf45959d8f6fa87
Grif-cpp <71193593+Grif-cpp@users.noreply.github.com>
2020-12-21 17:43:31 +0300

    Update length.h

--- module-1/homework/TypeList/typelist/length.h
+++ module-1/homework/TypeList/typelist/length.h
@@ -2,5 +2,11 @@
 
 #include "typelist.h"
 
-template<typename TList> 
-struct Length;
+template<typename TList>
+struct Length {
+	static const int length = Length<typename TList::Tail>::length>+1;
+};
+template<>
+struct Length<NullType> {
+	static const int length = 0;
+};
