ca06429b9baffa504682f6892a553bb5d97e28f4
Grif-cpp <71193593+Grif-cpp@users.noreply.github.com>
2020-12-21 21:38:15 +0300

    Update biginteger.h
    update BigInteger.h

--- module-1/homework/BigInteger/biginteger.h
+++ module-1/homework/BigInteger/biginteger.h
@@ -1,34 +1,34 @@
-class bigint
+class BigInteger
 {
 private:
 std::vector<int>arr;
 	bool is_neg;
 public:
 	int base = 1e9;
-	friend bigint operator - (bigint a);
-	friend bigint operator +(const bigint a,const bigint b);
-	friend bigint operator -( bigint a,  bigint b);	
-	friend bigint Karatsuba(const bigint &a, const bigint &b);
-	friend void extend_vec(bigint &a, bigint &b);
-	friend bigint naive_mul(const bigint & x, const bigint& y);
-	friend bigint operator *(const bigint &a,const bigint &b);
-	friend bigint operator /( bigint &a,  bigint &b);
-	friend bool operator <(const bigint &a,const bigint &b);
-	friend void operator ++(bigint &a);
-	friend void operator --(bigint &a);
-	friend bool operator >(bigint &a, bigint &b);
-	friend bool operator <=(bigint &a, bigint &b);
-	friend bool operator >=(bigint &a, bigint &b);
-	friend bool operator ==(bigint &a, bigint &b);
-	friend bool operator !=(bigint &a, bigint &b);
-	friend std::ostream& operator <<(std::ostream& os, bigint& a);
-	friend std::ostream& operator <<(std::ostream& os, bigint&& a);
-	friend std::istream& operator >>(std::istream& os, bigint& a);
-	friend bigint toBigint(std::string& s);
-	friend std::string toString(bigint &a);
-	friend bigint toBigint(int& a);
-	friend bool toBool(bigint &a);
-	void dig() {
+	friend BigInteger operator - (BigInteger a);
+	friend BigInteger operator +(const BigInteger a,const BigInteger b);
+	friend BigInteger operator -( BigInteger a,  BigInteger b);	
+	friend BigInteger Karatsuba(const BigInteger &a, const BigInteger &b);
+	friend void extend_vec(BigInteger &a, BigInteger &b);
+	friend BigInteger naive_mul(const BigInteger & x, const BigInteger& y);
+	friend BigInteger operator *(const BigInteger &a,const BigInteger &b);
+	friend BigInteger operator/( BigInteger &a,  BigInteger &b);
+	friend bool operator <(const BigInteger &a,const BigInteger &b);
+	friend void operator ++(BigInteger &a);
+	friend void operator --(BigInteger &a);
+	friend bool operator >(const BigInteger &a, const BigInteger &b);
+	friend bool operator <=(const BigInteger &a, const BigInteger &b);
+	friend bool operator >=(const BigInteger &a, const BigInteger &b);
+	friend bool operator ==(const BigInteger &a, const BigInteger &b);
+	friend bool operator !=(const BigInteger &a, const BigInteger &b);
+	friend std::ostream& operator <<(std::ostream& os, BigInteger& a);
+	friend std::ostream& operator <<(std::ostream& os, BigInteger&& a);
+	friend std::istream& operator >>(std::istream& os, BigInteger& a);
+	friend BigInteger toBigint(std::string& s);
+	friend std::string toString(BigInteger &a);
+	friend BigInteger toBigint(int& a);
+	friend bool toBool(BigInteger &a);
+	void resizer() {
 		for (auto i = arr.size() - 1; i >= 0; i--) {
 			if (arr[i] != 0) {
 				arr.resize(i + 1);
